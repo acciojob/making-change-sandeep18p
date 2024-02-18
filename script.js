811974170187981a898f6106ba54929c1c7d28ff
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-02-18 23:21:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const makeChange = (c) => {
-  // your name here
+ const quarters = Math.floor(c / 25); // Number of quarters
+  c %= 25; // Update c to the remainder after using quarters
+
+  const dimes = Math.floor(c / 10); // Number of dimes
+  c %= 10; // Update c to the remainder after using dimes
+
+  const nickels = Math.floor(c / 5); // Number of nickels
+  c %= 5; // Update c to the remainder after using nickels
+
+  const pennies = c; // Remaining pennies
+
+  return { "q": quarters, "d": dimes, "n": nickels, "p": pennies };
 };
 
 // Do not the change the code below
